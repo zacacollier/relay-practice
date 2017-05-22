34289595d83f77b45cf779d4f43b2272dab3b46c
Zac Collier <zacacollier@gmail.com>
2017-05-21 19:32:20 -0500

    Add server, eslint

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const app = express();
+
+const PORT = 1337;
+
+app.use('/graphql', expressGraphQL(
+  {
+  graphiql: true,
+  }
+));
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
