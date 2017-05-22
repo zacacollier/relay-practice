7c775cdf1d86c8ed2a65dea221362295dbebcfff
Zac Collier <zacacollier@gmail.com>
2017-05-22 10:59:29 -0500

    schema: add axios

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 
 const {
   GraphQLObjectType,
@@ -27,7 +27,8 @@ const RootQuery = new GraphQLObjectType(
         type: UserType,
         args: { id: { type: GraphQLString } },
         resolve(parentValue, args) {
-          return _.find(users, { id: args.id });
+          return axios.get(`http://localhost:3000/users/${args.id}`)
+            .then(res => res.data);
         },
       },
     },
